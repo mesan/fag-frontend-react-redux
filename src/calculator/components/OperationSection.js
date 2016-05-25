8c9ce6298fbad837725b754bab0f97ff218c7ca2
Arild Wanvik Tvergrov <arildt@mesan.no>
2016-05-25 14:29:22 +0200

    Remove operations array to make code more readable.

--- src/calculator/components/OperationSection.js
+++ src/calculator/components/OperationSection.js
@@ -1,22 +1,24 @@
 import React, { PropTypes } from 'react';
 
-const OperationSection = ({ nextOperand, operations, operationActions }) =>
+const OperationSection = ({
+  nextOperand,
+  operationActions: {
+    add,
+    subtract,
+    multiply,
+    divide
+  }
+}) =>
   <section className="operation-buttons">
-    {operations.map((operation, index) =>
-      <button
-        key={operation}
-        onClick={operationActions[index](nextOperand)}
-        className="button operation-button"
-      >
-        {operation}
-      </button>
-    )}
+    <button onClick={add(nextOperand)} className="button operation-button">+</button>
+    <button onClick={subtract(nextOperand)} className="button operation-button">−</button>
+    <button onClick={multiply(nextOperand)} className="button operation-button">×</button>
+    <button onClick={divide(nextOperand)} className="button operation-button">÷</button>
   </section>;
 
 OperationSection.propTypes = {
   nextOperand: PropTypes.number.isRequired,
-  operations: PropTypes.array.isRequired,
-  operationActions: PropTypes.array.isRequired
+  operationActions: PropTypes.object.isRequired
 };
 
 export default OperationSection;
--- src/calculator/containers/CalculatorContainer.js
+++ src/calculator/containers/CalculatorContainer.js
@@ -7,23 +7,21 @@ import multiplyOperand from '../actionCreators/multiplyOperand';
 import divideOperand from '../actionCreators/divideOperand';
 
 const digits = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
-const operations = ['+', '−', '×', '÷'];
 
 const mapStateToProps = ({ sum, nextOperand }) => ({
   sum,
   nextOperand,
-  digits,
-  operations
+  digits
 });
 
 const mapDispatchToProps = dispatch => ({
   addDigitToOperand: number => () => dispatch(addDigitToOperand(number)),
-  operationActions: [
-    operand => () => dispatch(addOperand(operand)),
-    operand => () => dispatch(subtractOperand(operand)),
-    operand => () => dispatch(multiplyOperand(operand)),
-    operand => () => dispatch(divideOperand(operand))
-  ]
+  operationActions: {
+    add: operand => () => dispatch(addOperand(operand)),
+    subtract: operand => () => dispatch(subtractOperand(operand)),
+    multiply: operand => () => dispatch(multiplyOperand(operand)),
+    divide: operand => () => dispatch(divideOperand(operand))
+  }
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Calculator);
